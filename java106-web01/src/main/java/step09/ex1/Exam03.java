4bda676df747391a8373667d68c371a2700fd86e
gilnami <gilnami.kim@gmail.com>
2018-05-21 12:48:14 +0900

    .

--- java106-web01/src/main/java/step09/ex1/Exam03.java
+++ java106-web01/src/main/java/step09/ex1/Exam03.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,15 +13,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
-@WebServlet("/step09/ex1/exam03")
+// 애노테이션 대신 web.xml에 배치정보를 설정한다.
+// @WebServlet("/step09/ex1/exam03")
 public class Exam03 extends HttpServlet {
     @Override
     protected void doGet(
             HttpServletRequest request, 
             HttpServletResponse response) throws ServletException, IOException {
         
-        request.setCharacterEncoding("UTF-8");
-        String name = request.getParameter("name");
         
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
@@ -33,6 +33,15 @@ public class Exam03 extends HttpServlet {
         out.println("</head>");
         out.println("<body>");
         out.println("<h1>exam03 실행!</h1>\n");
+        
+        // init()의 파리미터로 받은 ServletConfig 객체를 이용하여
+        // web.xml 파일에 설정된 init-param 태그의 값을 가져올 수 있다.
+        ServletConfig config = this.getServletConfig();
+        
+        String name = config.getInitParameter("name");
+        String age = config.getInitParameter("age");
+        
+        out.printf("<p>name=%s, age=%s</p>\n", name, age);
         out.println("</body>");
         out.println("</html>");
         
