5c0fb021ff5bf0bee08472071bfa8cff5ec7c34c
gilnami <gilnami.kim@gmail.com>
2018-05-23 11:03:48 +0900

    .

--- java106-web01/src/main/java/step10/ex2/Exam01.java
+++ java106-web01/src/main/java/step10/ex2/Exam01.java
@@ -1,4 +1,4 @@
-// 쿠키 : 클라이언트쪽에 데이터를 보관하는 방법
+// 쿠키 : 유효기간 설정하기
 package step10.ex2;
 
 import java.io.IOException;
@@ -12,36 +12,40 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
-@WebServlet("/step10/ex1/exam01")
+@WebServlet("/step10/ex2/exam01")
 public class Exam01 extends HttpServlet {
     @Override
     protected void doGet(
             HttpServletRequest request, 
             HttpServletResponse response) throws ServletException, IOException {
         
-        // 쿠키(cookie) 보내기
-        
-        // 1) key-value 한 쌍의 값을 갖는 쿠키 객체를 만든다.
         Cookie cookie1 = new Cookie("c1", "aaa");
-        Cookie cookie2 = new Cookie("c2", "123");
+        cookie1.setMaxAge(20); // 유지 시간(초)을 설정한다.
+        // 유효기간을 설정하면 웹브라우저를 닫아도, 컴퓨터를 종료해도 유지된다.
+        // 왜? 파일시스템(HDD 등)에 보관하기 때문에 컴퓨터를 꺼도 유지된다.
+        // 당연히 유효기간이 지나면 웹브라우저는 웹서버에 보내지 않고 해당 쿠키를 제거한다.
+        
+        Cookie cookie2 = new Cookie("c2", "bbb");
+        cookie2.setMaxAge(40); // 30초 동안만 유효하다.
+        
+        Cookie cookie3 = new Cookie("c3", "ccc");
+        // 유효기간을 설정하지 않으면 웹브라우저를 실행하는 동안만 유효하다.
+        // 웹브라우저를 닫으면 유효기간이 설정되지 않은 쿠키는 자동으로 삭제된다.
         
         // 2) 응답 헤더에 쿠키정보를 붙인다.
         response.addCookie(cookie1);
         response.addCookie(cookie2);
+        response.addCookie(cookie3);
         
         response.setContentType("text/plain;charset=utf-8");
         PrintWriter out = response.getWriter();
         
         out.println("쿠키를 보냈습니다");
         
-        // 쿠키(cookie)?
-        // => 웹서버가 보낸 데이터를 웹브라우저쪽에 저장하는 것
-        // => 웹브라우저는 웹서버로부터 받은 데이터를 보관하고 있다가
-        //    웹서버에 요청할 때마다 다시 보낸다.
-        //    예) 쿠폰
-        //
         // 쿠키를 클라이언트로 보내는 HTTP 프로토콜?
         /*
+        * 유효기간을 설정안했을 떄
+        * 
         HTTP/1.1 200
         Set-Cookie: c1=aaa
         Set-Cookie: c2=123
@@ -50,6 +54,15 @@ public class Exam01 extends HttpServlet {
         Date: Wed, 23 May 2018 01:17:15 GMT
         (빈 줄)
         -내용-
+        
+        * 유효기간을 설정했을 때
+        HTTP/1.1 200
+        Set-Cookie: c1=aaa; Max-Age=10; Expires=Wed, 23-May-2018 01:54:00 GMT
+        Set-Cookie: c2=bbb; Max-Age=30; Expires=Wed, 23-May-2018 01:54:20 GMT
+        Set-Cookie: c3=ccc
+        Content-Type: text/plain;charset=utf-8
+        Content-Length: 27
+        Date: Wed, 23 May 2018 01:53:50 GMT
         */
     }
 }
