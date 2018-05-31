fd0deaa626b9d1855682d7f5d1db44d287dd5205
gilnami <gilnami.kim@gmail.com>
2018-05-31 10:37:03 +0900

    .

--- bitcamp-spring-ioc/src/main/java/bitcamp/java106/step12/ex1/Exam01.java
+++ bitcamp-spring-ioc/src/main/java/bitcamp/java106/step12/ex1/Exam01.java
@@ -1,3 +1,4 @@
+// Spring과 Mybatis 연동 : 단순 연동
 package bitcamp.java106.step12.ex1;
 
 import java.util.List;
@@ -10,7 +11,8 @@ import bitcamp.java106.step12.Board;
 
 public class Exam01 {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        
         ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
                 "bitcamp/java106/step12/ex1/application-context.xml");
         
@@ -18,21 +20,22 @@ public class Exam01 {
         
         BoardDao boardDao = iocContainer.getBean(BoardDao.class);
         
-        // 1) 게시물 입력
+        //1) 게시물 입력
         Board board = new Board();
-        board.setTitle("제목입니다");
+        board.setTitle("제목입니다.");
         board.setContent("내용입니다.");
         boardDao.insert(board);
-
-        // 2) 게시물 목록 조회
+        
+        //2) 게시물 목록 조회
         List<Board> list = boardDao.selectList(1, 5);
         for (Board b : list) {
-            System.out.printf("%s, %s ,%s\n", 
-                    b.getNo(), 
-                    b.getTitle(), 
+            System.out.printf("%d, %s, %s\n", 
+                    b.getNo(),
+                    b.getTitle(),
                     b.getRegisteredDate());
         }
     }
+
 }
 
 
