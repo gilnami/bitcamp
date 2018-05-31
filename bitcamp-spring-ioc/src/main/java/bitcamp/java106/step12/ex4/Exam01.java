eb16419cf08cd275a9050f5a50ecfb9a87563523
gilnami <gilnami.kim@gmail.com>
2018-05-31 12:45:08 +0900

    .

--- bitcamp-spring-ioc/src/main/java/bitcamp/java106/step12/ex4/Exam01.java
+++ bitcamp-spring-ioc/src/main/java/bitcamp/java106/step12/ex4/Exam01.java
@@ -1,6 +1,7 @@
-// Spring과 Mybatis 연동 : mybatis에서 제공해주는 spring 연동 라이브러리 사용하기
-package bitcamp.java106.step12.ex3;
+// Spring과 Mybatis 연동 : mybatis가 구현한 DAO 객체 사용하기
+package bitcamp.java106.step12.ex4;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.context.ApplicationContext;
@@ -21,7 +22,7 @@ public class Exam01 {
         // 4) 프로젝트 갱신
         
         ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
-                "bitcamp/java106/step12/ex3/application-context.xml");
+                "bitcamp/java106/step12/ex4/application-context.xml");
         
         BeanUtils.printBeanNames(iocContainer);
         
@@ -29,12 +30,17 @@ public class Exam01 {
         
         //1) 게시물 입력
         Board board = new Board();
-        board.setTitle("왜왜왜");
-        board.setContent("뭔데?");
+        board.setTitle("DAO구현 해방!");
+        board.setContent("해방!!!");
         boardDao.insert(board);
         
         //2) 게시물 목록 조회
-        List<Board> list = boardDao.selectList(1, 5);
+        // => selectList()의 파리미터 값을 한 개만 넘겨야 하기 때문에
+        //    여러 개의 값을 넣고 싶으면 Map에 담아 넘긴다.
+        HashMap<String,Object> params = new HashMap<>();
+        params.put("startIndex", 0);
+        params.put("pageSize", 5);
+        List<Board> list = boardDao.selectList(params);
         for (Board b : list) {
             System.out.printf("%d, %s, %s\n", 
                     b.getNo(),
