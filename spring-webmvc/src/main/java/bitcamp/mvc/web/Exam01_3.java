b7f95f1804d294a64bba9a99844000625d4afd9d
gilnami <gilnami.kim@gmail.com>
2018-06-04 09:58:20 +0900

    .

--- spring-webmvc/src/main/java/bitcamp/mvc/web/Exam01_3.java
+++ spring-webmvc/src/main/java/bitcamp/mvc/web/Exam01_3.java
@@ -6,19 +6,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/exam03") // 클래스 선언부에는 상위 URL을 두고
-public class Exam03 {
+@RequestMapping("/exam01_3") // 클래스 선언부에는 상위 URL을 두고
+public class Exam01_3 {
     
     // 각각의 RequestMapping 애노테이션에 하위 URL을 정의할 수 있다.
     @RequestMapping("m1")
     @ResponseBody
     public String m1() {
-        return "Exam03.m1()";
+        return "Exam01_3.m1()";
     }
     
     @RequestMapping("m2")
     @ResponseBody
     public String m2() {
-        return "Exam03.m2()";
+        return "Exam01_3.m2()";
     }
 }
