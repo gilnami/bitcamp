9c331f52d41a74ab82810f866fba06ab987c0a2b
gilnami <gilnami.kim@gmail.com>
2018-06-05 16:18:03 +0900

    .

---
+++ spring-webmvc/src/main/java/bitcamp/mvc/web/Exam06_4.java
@@ -0,0 +1,26 @@
+// view resolver 다루기 - 기본 뷰 리졸버를 InternalResourceViewResolver
+package bitcamp.mvc.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller 
+@RequestMapping("/exam06_4") 
+public class Exam06_4 {
+    
+    @GetMapping(value="m1")  
+    public String m1(Model model) {
+        model.addAttribute("name","홍길동");
+        model.addAttribute("age",20);
+        return "/WEB-INF/jsp/exam06_4.jsp";
+    }
+}
+
+
+
+
+
+
+
---
+++ spring-webmvc/src/main/webapp/footer.jsp
@@ -0,0 +1,9 @@
+<%@ page
+    language="java" 
+    contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<div style="background-color: gray; color: black; text-align: center;">
+    <p>비트캠프 서초동</p>
+</div>
+
