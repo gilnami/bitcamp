c7e9640f877696d2cba1a35d18a8dd6a5dbe89f7
gilnami <gilnami.kim@gmail.com>
2018-06-08 11:50:06 +0900

    .

--- spring-webmvc/src/main/java/bitcamp/mvc/web/Exam09_1_interceptor.java
+++ spring-webmvc/src/main/java/bitcamp/mvc/web/Exam09_1_interceptor.java
@@ -11,6 +11,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+// 인터셉터(interceptor)
+// => Servlet의 Filter는 servlet 실행 전후에 개입하는 객체이다.
+// =>interceptor는 pageController 실행 전후와 JSP 실행 전후에 개입하는 객체이다.
+// => interceptor는 HandlerInterceptor를 구현해야 한다.
+// => 배치 방법?
+//   1) 자바 코드
+//   2) XML : mvc-servlet.xml을 참고
+// => 사용 예)
+//    - 사용자 인증 또는 권한 검사
+//    - log를 남길때 
+//    - pageController를 실행하기 전에 client가 보낸 data를 암호 해제할 때
+//    - client에게 보낼 data를 암호화시킬 때
+//    - content의 암축 또는 해제
+
 public class Exam09_1_interceptor implements HandlerInterceptor{
     @Override
     public boolean preHandle(
