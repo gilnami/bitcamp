f7ea5c94eafddd0dcef5a054dbb676e3b3563075
gilnami <gilnami.kim@gmail.com>
2018-06-08 10:02:42 +0900

    .

--- spring-webmvc/src/main/java/bitcamp/mvc/web/Exam08_1.java
+++ spring-webmvc/src/main/java/bitcamp/mvc/web/Exam08_1.java
@@ -1,5 +1,7 @@
-// URL에서 값을 추출하기 - 정규표현식으로 값 추출하기
+// 세션 다루기 - http세션에 데이터 보관
 package bitcamp.mvc.web;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,32 +13,43 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/exam08_1") 
 public class Exam08_1 {
     
-    @GetMapping(value="m1/{value}", produces="text/plain;charset=UTF-8")  
+    @GetMapping(value="m1", produces="text/plain;charset=UTF-8")  
     @ResponseBody
     public String m1(
-            @PathVariable String value
+            String name,
+            HttpSession session
             ) {
-        // 예) 클라이언트에서 URL에 다음과 같은 형식으로 데이터를 보낼 때,
-        //    .../exam07_3/m1/teamA-hong-100
-        return String.format("m1(): value=%s", value);
+        // 테스트
+        //   .../mvc/exam08_1/m1?name=hong
+        // 세션에 데이터 보관
+        session.setAttribute("name", name);
+        return String.format("m1(): name=%s", name);
     }
     
-    @GetMapping(
-            value="m2/{team:t[a-zA-Z]+}-{member:m[a-z0-9]+}-{doc:[0-9]+}", 
-            produces="text/plain;charset=UTF-8")
-    // URI가 특정 규칙과 일치할 때 메서드를 호출하게 할 수 있다.
-    // 정규표현식으로 그 규칙을 제어한다.
+    @GetMapping(value="m2", produces="text/plain;charset=UTF-8")  
     @ResponseBody
     public String m2(
-            @PathVariable String team,
-            @PathVariable String member,
-            @PathVariable int doc
+            int age,
+            HttpSession session
             ) {
-        
-        
-        // 예) 클라이언트에서 URL에 다음과 같은 형식으로 데이터를 보낼 때,
-        //    .../exam07_3/m2/teamA-hong-100
-        return String.format("m2(): team=%s, member=%s, doc=%d", team, member, doc);
+        // 테스트
+        //   .../mvc/exam08_1/m2?age=20
+        // 세션에 데이터 보관
+        session.setAttribute("age", age);
+        return String.format("m2(): age=%d", age);
+    }
+    
+    @GetMapping(value="m3", produces="text/plain;charset=UTF-8")  
+    @ResponseBody
+    public String m3(
+            boolean working,
+            HttpSession session
+            ) {
+        // 테스트
+        //   .../mvc/exam08_1/m3?working=true
+        // 세션에 데이터 보관
+        session.setAttribute("working", working);
+        return String.format("m3(): working=%b", working);
     }
 }
 
