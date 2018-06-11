9b3aba3ce1948f4147c49bc62489c20894db7a6d
gilnami <gilnami.kim@gmail.com>
2018-06-11 16:39:04 +0900

    .

--- bitcamp-java-basic/src/step25/ex9/Exam01_insert.java
+++ bitcamp-java-basic/src/step25/ex9/Exam01_insert.java
@@ -9,7 +9,7 @@ public class Exam01_insert {
         
         ClassPathXmlApplicationContext iocContainer = 
                 new ClassPathXmlApplicationContext(
-                        "step25/ex8/application-context.xml");
+                        "step25/ex9/application-context.xml");
         
         // 트랜잭션 관리자를 도입하지 않으면,
         // DAO의 각 메서드 호출은 독립작업으로 간주한다.
@@ -17,6 +17,10 @@ public class Exam01_insert {
         // DataSource로 부터 별도의 DB 커넥션을 받아서 사용한다.
         // 따라서 DAO 메서드를 호출하다가 오류가 발생하더라도,
         // 그 전에 호출한 메서드의 작업은 그대로 DB 테이블에 적용된다.
+        
+        // 트렌젝션 적용
+        // 1) 트랜잭션 관리자를 등록해야한다.
+        //    => application-context
         BoardDao boardDao = iocContainer.getBean(BoardDao.class);
                 
         
