7bbc911fc56bc8177637921c97f5f5650fa4f5e6
gilnami <gilnami.kim@gmail.com>
2018-07-24 14:34:31 +0900

    .

--- tamlaisjeju_gilnam/src/main/java/com/WeatherController2.java
+++ tamlaisjeju_gilnam/src/main/java/com/WeatherController2.java
@@ -6,6 +6,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
@@ -17,17 +18,17 @@ import org.json.simple.parser.JSONParser;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/weather")
-public class WeatherController {
+//@RestController
+//@RequestMapping("/tour")
+public class WeatherController2 {
     
-    @RequestMapping("info")
-    public Object weather(int nx, int ny) {
+    //@RequestMapping("info")
+    public static void main(String[] args) {
         BufferedReader br = null;
         
         //현재 날짜 받아오기
         SimpleDateFormat day = new SimpleDateFormat("yyyyMMdd", Locale.KOREA);
-        SimpleDateFormat hr = new SimpleDateFormat("HH", Locale.KOREA);
+        SimpleDateFormat hr = new SimpleDateFormat("hh", Locale.KOREA);
         SimpleDateFormat min = new SimpleDateFormat("mm", Locale.KOREA);
         Calendar c1 = Calendar.getInstance();
         int today = Integer.parseInt(day.format(c1.getTime()));
@@ -50,21 +51,38 @@ public class WeatherController {
         } else {
             base_time = hours + "00";
         }
+        System.out.println(base_time);
+        
+        
+        
         
         try {
-            String urlstr = "http://newsky2.kma.go.kr/"
-                    + "service/SecndSrtpdFrcstInfoService2/ForecastGrib"
+            String urlstr = "http://api.visitkorea.or.kr/openapi/"
+                    + "service/rest/KorService/areaBasedList"
                     + "?ServiceKey=j2KT0WiQsYIQ1daSvlJI9F2%2Be0F0UW%2BacYymDWHHCX3BTq8ObPmb7WJSFxmIkLzxJDpIG%2BgjCRY1GqbWupOSlQ%3D%3D"
-                    + "&base_date="
-                    + today
-                    + "&base_time="
-                    + base_time
-                    + "&nx=" + nx
-                    + "&ny=" + ny
-                    + "&numOfRows=10"
-                    + "&pageNo=1"
+                    + "&pageNo="
+                    + "1"
+                    + "&startPage="
+                    + "1"
+                    + "&numOfRows="
+                    + "100"
+                    + "&pageSize="
+                    + "100"
+                    + "&MobileApp="
+                    + "AppTest"
+                    + "&MobileOS="
+                    + "ETC"
+                    + "&arrange="
+                    + "A"
+                    + "&contentTypeId="
+                    + "15"
+                    + "&areaCode="
+                    + "39"
+                    + "&listYN="
+                    + "Y"
                     + "&_type=json";
             URL url = new URL(urlstr);
+            //System.out.println(urlstr);
             HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
             urlconnection.setRequestMethod("GET");
             br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(), "UTF-8"));
@@ -74,7 +92,7 @@ public class WeatherController {
             while((line = br.readLine()) != null) {
                 result = result + line + "\n";
             }
-            
+            // System.out.println(result);
             
             JSONParser parser = new JSONParser();
             JSONObject obj = (JSONObject) parser.parse(result);
@@ -89,41 +107,27 @@ public class WeatherController {
             // items로 부터 itemlist 를 받아오기 itemlist : 뒤에 [ 로 시작하므로 jsonarray이다
             JSONArray parse_item = (JSONArray) parse_items.get("item");
             
-            JSONObject vec_item = (JSONObject)parse_item.get(7); // 풍향
-            JSONObject sky_item = (JSONObject)parse_item.get(4); // 하늘상태
-            JSONObject rain_item = (JSONObject)parse_item.get(1); // 강수형태
-            JSONObject temp_item = (JSONObject)parse_item.get(5); // 기온
-            JSONObject wsd_item = (JSONObject)parse_item.get(9); // 풍속
             
-            long vec = (Long) vec_item.get("obsrValue");
-            String vector;
             
-            if(337 < vec && vec < 23) {
-                vector = "북";
-            } else if(22 < vec && vec < 68) {
-                vector = "북동";
-            } else if(67 < vec && vec < 113) {
-                vector = "동";
-            } else if(112 < vec && vec < 158) {
-                vector = "남동";
-            } else if(157 < vec && vec < 203) {
-                vector = "남";
-            } else if(202 < vec && vec < 248) {
-                vector = "남서";
-            } else if(247 < vec && vec < 293) {
-                vector = "서";
-            } vector = "북서";
             
-            map.put("rain", rain_item.get("obsrValue").toString());
-            map.put("sky", sky_item.get("obsrValue").toString());
-            map.put("temp", temp_item.get("obsrValue").toString());
-            map.put("vec", vector);
-            map.put("wsd", wsd_item.get("obsrValue").toString());
+            
+            
+            
+            
+            
+            /*for(int i = 0; i < parse_item.size(); i++) {
+                JSONObject content = (JSONObject) parse_item.get(i);
+                long id = (Long) content.get("contentid");
+                if(527505L == id) {
+                    System.out.println(content);
+                }
+            }*/
+            
                 br.close();
             }catch(Exception e){
                 System.out.println(e);
             }
-        return map;
+        System.out.println();
         }
-    
+     
     }
